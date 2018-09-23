f1464726e3974f8dac668175399748639d6ab996
Stanislav Voroniy <stas@voroniy.com>
2018-09-24 01:23:08 +0200

    Update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
 name 'clamav-milter'
 maintainer 'Stanislav Voroniy'
 maintainer_email 'stas@voroniy.com'
-license 'Apache v2.0'
+license 'Apache-2.0'
 description 'Installs/Configures clamav-milter'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.3'
+version '0.4.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
 issues_url 'https://github.com/voroniys/clamav-milter/issues' if respond_to?(:source_url)
 source_url 'https://github.com/voroniys/clamav-milter' if respond_to?(:issues_url)
 
